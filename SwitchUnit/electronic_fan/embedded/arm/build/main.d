96b552b610f88e86b7ee90f30fb6a3fff4e14f3f
qian.yao <qian.yao@jumacc.com>
2015-07-14 16:34:50 +0800

    Add template folder

--- SwitchUnit/electronic_fan/embedded/arm/build/main.d
+++
@@ -1,7 +0,0 @@
-.\build\main.o: ..\main.c
-.\build\main.o: ..\..\..\sdk\include\juma_sdk_api.h
-.\build\main.o: ..\..\..\sdk\include\juma_sdk_types.h
-.\build\main.o: C:\nigel\software\keil_mdk514\ARM\ARMCC\Bin\..\include\stdio.h
-.\build\main.o: C:\nigel\software\keil_mdk514\ARM\ARMCC\Bin\..\include\stdint.h
-.\build\main.o: C:\nigel\software\keil_mdk514\ARM\ARMCC\Bin\..\include\stdlib.h
-.\build\main.o: C:\nigel\software\keil_mdk514\ARM\ARMCC\Bin\..\include\string.h
